e796a5d552194b7b994690e1c7dd7b33b6f6012e
teddy0818d <taddy0818@naver.com>
2021-01-02 19:11:12 +0900

    Step7_1065

---
+++ Algorithm/src/com/hbj/step7/Step7_11654.java
@@ -0,0 +1,17 @@
+package com.hbj.step7;
+
+import java.util.Scanner;
+
+public class Step7_11654 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		char input = scan.nextLine().charAt(0);
+		int num = (int)input;
+		
+		System.out.println(num);
+
+	}
+	
+}
