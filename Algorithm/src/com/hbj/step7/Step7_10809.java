57a83a09b95fccdb15e5549b7036dcc33720fc2c
teddy0818d <taddy0818@naver.com>
2021-01-07 21:45:35 +0900

    Step7_10809

---
+++ Algorithm/src/com/hbj/step7/Step7_10809.java
@@ -0,0 +1,36 @@
+//a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+package com.hbj.step7;
+
+import java.util.Scanner;
+
+public class Step7_10809 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.next();
+		
+		String[] arr1 = {"a", "b" ,"c", "d","e", "f", "g", "h", "i", "j", "k", "l" ,"m","n", "o", "p", "q", "r", "s", "t", "u", "v" ,"w"
+				, "x", "y", "z"};
+		int[] arr2 = new int[arr1.length];
+		
+		for(int i=0; i<arr2.length; i++) {
+			arr2[i] = -1;
+		}
+		
+		for(int i=0; i<input.length(); i++) {
+			for(int z=0; z<arr2.length; z++) {
+				if(input.subSequence(i, i+1).equals(arr1[z])) {
+					arr2[z] += 1;
+				}
+			}
+		}
+		
+		for(int i=0; i<arr2.length; i++) {
+			System.out.print(arr2[i] + " ");
+		}
+
+	}
+
+}
