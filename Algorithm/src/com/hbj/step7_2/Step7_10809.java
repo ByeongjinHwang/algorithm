88b49160069d151bdf394f8f705c3a8a2d9cc021
Byeongjin Hwang <taddy0818@naver.com>
2021-02-15 20:41:04 +0900

    Step7_10809 푸는

---
+++ Algorithm/src/com/hbj/step7_2/Step7_10809.java
@@ -0,0 +1,20 @@
+package com.hbj.step7_2;
+
+import java.util.Scanner;
+
+public class Step7_10809 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.next();
+		
+		int[] arr = new int[24]; // 알파벳 24개 저장하는 배
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = -1; // 배열의 각 방에 -1값 넣
+		}
+		
+	}
+
+}
