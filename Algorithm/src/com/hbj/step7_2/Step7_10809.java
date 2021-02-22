92e6baac0243d245476cffae301ed6728ceb7e35
Byeongjin Hwang <taddy0818@naver.com>
2021-02-22 11:00:57 +0900

    Step7_10809 완료

--- Algorithm/src/com/hbj/step7_2/Step7_10809.java
+++ Algorithm/src/com/hbj/step7_2/Step7_10809.java
@@ -12,7 +12,20 @@ public class Step7_10809 {
 		int[] arr = new int[24]; // 알파벳 24개 저장하는 배
 		
 		for(int i=0; i<arr.length; i++) {
-			arr[i] = -1; // 배열의 각 방에 -1값 넣
+			arr[i] = -1; // 배열의 각 방에 -1값 넣음 
+		}
+		
+		for(int i=0; i<str.length(); i++) {
+			int temp = (int) str.charAt(i);
+			for(int z=0; z<arr.length; z++) {
+				if(temp-97 == z && arr[z] == -1) {
+					arr[z] = i;
+				}
+			}
+		}
+	
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		
 	}
