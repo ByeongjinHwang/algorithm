c0e85d19881ebcd3f48effa6d9a50e81c259e351
Byeongjin Hwang <taddy0818@naver.com>
2021-02-25 19:42:22 +0900

    Step7_2675 수정

--- Algorithm/src/com/hbj/step7_2/Step7_2675.java
+++ Algorithm/src/com/hbj/step7_2/Step7_2675.java
@@ -7,17 +7,22 @@ public class Step7_2675 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int tn = scan.nextInt();
-		
+		String result = ""; 
 		
 		for(int i=0; i<tn; i++) {
-			System.out.println();
+			
+			result = ""; 
+			
 			int num = scan.nextInt();
 			String str = scan.next();
+			
 			for(int j=0; j<str.length(); j++) {
-				for(int x=0; x<num; num++) {
-					System.out.print(str.substring(j, j+1));
+				for(int x=0; x<num; x++) {
+					result += str.substring(j, j+1);
 				}
 			}
+			
+			System.out.println(result);
 		}
 	}
 
