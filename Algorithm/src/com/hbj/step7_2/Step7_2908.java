4cceecd6e3dfe1d205f3cb4631dcb3ee903c9a30
Byeongjin Hwang <taddy0818@naver.com>
2021-02-28 20:47:27 +0900

    Step7_2908

---
+++ Algorithm/src/com/hbj/step7_2/Step7_2908.java
@@ -0,0 +1,17 @@
+package com.hbj.step7_2;
+
+import java.util.Scanner;
+
+public class Step7_2908 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String num1 = scan.next();
+		String num2 = scan.next();
+		
+		
+		
+	}
+
+}
