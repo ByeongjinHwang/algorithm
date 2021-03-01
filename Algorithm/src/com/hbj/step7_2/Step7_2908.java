cc341739bc5183e22e948ab065f7304407010620
Byeongjin Hwang <taddy0818@naver.com>
2021-03-01 09:10:13 +0900

    Step7_2908

--- Algorithm/src/com/hbj/step7_2/Step7_2908.java
+++ Algorithm/src/com/hbj/step7_2/Step7_2908.java
@@ -8,7 +8,21 @@ public class Step7_2908 {
 		Scanner scan = new Scanner(System.in);
 		
 		String num1 = scan.next();
+		String t_num1 = num1.substring(2, 3) + num1.substring(1, 2) + num1.substring(0, 1); 
+		int int_t_num1 = Integer.parseInt(t_num1);
+		
 		String num2 = scan.next();
+		String t_num2 = num2.substring(2, 3) + num2.substring(1, 2) + num2.substring(0, 1); 
+		int int_t_num2 = Integer.parseInt(t_num2);
+		
+		if(int_t_num1 > int_t_num2) {
+			System.out.println(int_t_num1);
+		} else {
+			System.out.println(int_t_num2);
+		}
+		
+		
+		
 		
 		
 		
