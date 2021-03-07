5a2cb734080f760c83ccbdd80f4655bd47b8d50a
Byeongjin Hwang <taddy0818@naver.com>
2021-03-07 15:02:17 +0900

    Step8_2292 Solving

--- Algorithm/src/com/hbj/step8/Step8_2292.java
+++ Algorithm/src/com/hbj/step8/Step8_2292.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 public class Step8_2292 {
 
 	public static void main(String[] args) {
-		// 벌집갯수 : 1 - 6 - 12 - 17
-		
+		// 벌집갯수 : 1(1) - 2~7(6) - 8~19(12) - 20~37(18) - 38~61(23)
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
+		int result = 0;
+		
+		
 		
 		
 	}
